118658009ced7d7b2ac15584f253af9f75cd91a1
ingcristiansuarez <ing.cristiansuarez@gmail.com>
2021-08-09 07:01:03 -0500

    clase 26, Aplicando herencia en lenguaje Java y PHP

--- PHP/Account.php
+++ PHP/Account.php
@@ -1,5 +1,4 @@
 <?php
-
 class Account {
     public $id;
     public $name;
@@ -7,9 +6,11 @@ class Account {
     public $email;
     public $password;
 
-    public funtion __construct($name, $document){
+    public function __construct($name, $document){
         $this->name = $name;
         $this->document = $document;
     }
 
 }
+
+?>
--- PHP/Car.php
+++ PHP/Car.php
@@ -1,19 +1,18 @@
 <?php
-
-class Car{
+require_once('account.php');
+class Car {
     public $id;
     public $license;
-    public $driver; 
-    public $passengers;
+    public $driver;
+    public $passenger;
 
     public function __construct($license, $driver){
         $this->license = $license;
-        $this->driver= $driver;
+        $this->driver = $driver;
     }
 
-    public function PrintDataCar(){
-        echo "License: $this->license, Conductor: {
-        $this->diver->name}, documente {$this->driver->document}";
+    public function printDataCar() {
+        echo "Licencia: $this->license Driver: ".$this->driver->name;
     }
-
 }
+?>
