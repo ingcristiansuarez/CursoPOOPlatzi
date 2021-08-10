d728dc21a62a3c5ba633709bbf67f50ec0830d4c
ingcristiansuarez <ing.cristiansuarez@gmail.com>
2021-08-09 20:55:58 -0500

    clase 32, Encapsulando atributos en Java

--- Java/Car.java
+++ Java/Car.java
@@ -1,15 +1,27 @@
 package CursoPOOUber.Java;
 
+
+
 class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passegenger;
+    private Integer passenger;
 
     public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
+        
     }
+
+    public Integer getPasseger(){
+        return passenger;
+    }
+    
+    public void setPassegenger(Integer passenger){
+        this.passenger = passenger;
+    }
+    
 
     void printDataCar(){
         System.out.println("licence: "+ license + " Nombre driver: "+ driver.name + " passenger: "+passegenger);
--- Java/Main.java
+++ Java/Main.java
@@ -3,12 +3,14 @@ package CursoPOOUber.Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        Car car = new Car("IIA244", new Account("Cosme Fulanito", "123455") );
-        car.passegenger = 4;
-        car.printDataCar();
-
+        UberX uberX = new UberX("IIA244", new Account("Cosme Fulanito", "123455"), "FORD", "FIESTA");
+      //  uberX.passegenger = 4;
+        uberX.setPassegenger(4);
+        uberX.printDataCar();
+/*
         Car car2 = new Car("AAA323", new Account("Homero Thompson", "432123") );
         car2.passegenger = 5;
         car2.printDataCar();
+        */
     }
 }
