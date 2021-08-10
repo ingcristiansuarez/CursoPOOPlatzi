d83aa109db64fdf3dd9a1a5dcac8dc36bc937ffa
ingcristiansuarez <ing.cristiansuarez@gmail.com>
2021-08-09 20:24:11 -0500

    clase 28, Aplicando herencia en lenguaje Python y JavaScript

--- JS/Index.js
+++ JS/Index.js
@@ -2,3 +2,7 @@
 var car= new Car("AAA123", new Account("Comes fulantio", "OOO123"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("QWE123", new Account ("Homero Thompson", "QWE123"), "Chevrolet", "Spark") ;
+uberX.passenger = 5;
+uberX.printDataCar();
--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,8 @@
 from car import Car
 from account import Account
 
+
+
 if __name__ == "__main__":
     print("hola Mundo")
     
